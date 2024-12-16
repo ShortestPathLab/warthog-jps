8d26209f740f6613569732ce37134a4a02d4a388
Ryan Hechenberger <Ryan.Hechenberger@proton.me>
2024-12-16 13:05:50 +1100

    removed jps direction in id

--- include/jps/forward.h
+++ include/jps/forward.h
@@ -10,7 +10,7 @@ namespace jps
 
 using warthog::pack_id;
 using warthog::pad_id;
-using jps_id = warthog::pad32_id;
+using jps_id = pad_id;
 
 typedef enum
 {
@@ -25,12 +25,6 @@ typedef enum
 	SOUTHWEST = 128,
 	ALL = 255
 } direction;
-
-// we sometimes store the id of a node in the lower 3 bytes of a word and
-// use the upper byte to store something else (e.g. the parent direction)
-[[deprecated]]
-constexpr uint32_t JPS_ID_MASK
-    = (1 << 24) - 1;
 
 using vec_jps_id = std::vector<jps_id>;
 using vec_jps_cost = std::vector<warthog::cost_t>;
--- src/search/jps2plus_expansion_policy.cpp
+++ src/search/jps2plus_expansion_policy.cpp
@@ -60,7 +60,7 @@ jps2plus_expansion_policy::expand(
 		jps_id jp_id = jp_ids_.at(i);
 		// TODO: FIX jps node id
 		warthog::search::search_node* mynode
-		    = generate(jps_id(jp_id.id & jps::JPS_ID_MASK));
+		    = generate(jps_id(jp_id.id));
 		add_neighbour(mynode, costs_.at(i));
 	}
 }
